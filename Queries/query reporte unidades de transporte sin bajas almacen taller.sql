42bc8684c8410e03dadee9d3640ff2581f7737eb
David Garcia <david.programming@gmail.com>
2015-01-28 13:02:08 -0600

    Cambios a queries para reportes de Unidades.

--- Queries/query reporte unidades de transporte sin bajas almacen taller.sql
+++ Queries/query reporte unidades de transporte sin bajas almacen taller.sql
@@ -1,1 +1,9 @@
-SELECT clave, numero_unidad, placas, modelo, fecha_inicio, IFNULL(fecha_fin,"ACTIVO") AS fecha_fin, (SELECT nombre FROM tipo_unidad WHERE tipo_unidad.id_tipo = unidad_transporte.id_tipo) AS tipo, (SELECT nombre FROM marca_unidad WHERE marca_unidad.id_marca = unidad_transporte.id_marca) AS marca FROM unidad_transporte WHERE status = TRUE ORDER BY numero_unidad;
+SELECT clave, no_economico, vin, placas, modelo, color, modelo_motor, 
+no_serie_motor, (SELECT nombre FROM marca_motor WHERE 
+marca_motor.id_marca_motor = unidad_transporte.id_marca_motor) 
+AS marca_motor, cpl, fecha_inicio, IFNULL(fecha_fin,"ACTIVO") AS 
+fecha_fin, (SELECT nombre FROM tipo_unidad WHERE 
+tipo_unidad.id_tipo = unidad_transporte.id_tipo) AS tipo, 
+(SELECT nombre FROM marca_unidad WHERE marca_unidad.id_marca = 
+unidad_transporte.id_marca) AS marca FROM unidad_transporte 
+WHERE status = TRUE ORDER BY clave;
